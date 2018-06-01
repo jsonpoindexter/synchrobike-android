5567e44b998ee6eef98b58945f42a950bb326a8b
Jason Poindexter <jason@launchbadge.com>
2018-06-01 15:59:35 -0700

    Add 'address' and 'port' editText to app

---
+++ app/src/androidTest/java/poindexter/json/synchrobike/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package poindexter.json.synchrobike
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("poindexter.json.synchrobike", appContext.packageName)
+    }
+}
